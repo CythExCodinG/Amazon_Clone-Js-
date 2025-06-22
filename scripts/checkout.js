2a7027a22ca2961e465e9d4da82a8bdff0e2d858
CythExCodinG <rohitiball256@gmail.com>
2025-06-22 17:13:56 +0530

    remove deleted element from html doc'

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -23,7 +23,8 @@ cart.forEach((cartItem) => {
   })
   html =
     `
-  <div class="cart-item-container">
+  <div class="cart-item-container 
+  js-cart-item-container-${id}">
           <div class="delivery-date">
             Delivery date: Tuesday, June 21
           </div>
@@ -102,6 +103,10 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
       let deleteId = link.dataset.productId;
       removeFromCart(deleteId);
-      console.log(cart)
+
+      const container = document.querySelector(`.js-cart-item-container-${deleteId}`)
+      console.log(container)
+      container.remove()
     })
   })
+
